bf7f0b22b5efa7661e7766fccf39bd7f9ee7ae67
long_nh <long_nh@flinters.vn>
2022-12-01 06:20:50 +0700

    add snake gym

--- get_gym.py
+++ get_gym.py
@@ -1,7 +1,21 @@
 import gym
 import flappy_bird_gym
+import gym_snake
+ 
+"""
+LIST OF TESTED GYMS:
+ - CartPole-v1
+ - FlappyBird-v0
+ - Snake-v0
+"""
 
 def make():
-    # env = gym.make("CartPole-v1")
-    env = flappy_bird_gym.make("FlappyBird-v0")
+    env = gym.make("Snake-v0")
+    # env.seed()
+    # env.set_rewards(rew_step=-4.9, rew_apple=50, rew_death=-30, rew_death2=-50)
     return env
+
+
+if __name__ == "__main__":
+    print(gym.envs.registry.all())
+    env = make()
--- gym_play.py
+++ gym_play.py
@@ -7,18 +7,18 @@ import torch.nn as nn
 from multiprocessing import Pool
 import get_gym
 
-STATE_DICT_PATH = './FlappyBird-v0.pth'
-env = get_gym.make()
 
+env = get_gym.make()
+STATE_DICT_PATH = f"./output/{env.unwrapped.spec.id}.pth"
 observation_space_size = env.observation_space.shape[0]
 action_space_size = env.action_space.n
 
 model = nn.Sequential(
     nn.Linear(observation_space_size, 16),
-    nn.ReLU(),
+    nn.Sigmoid(),
     nn.Linear(16, 16),
-    nn.ReLU(),
-    nn.Linear(16, action_space_size)
+    nn.Sigmoid(),
+    nn.Linear(16, action_space_size),
 )
 model.load_state_dict(torch.load(STATE_DICT_PATH))
 
@@ -33,10 +33,9 @@ with torch.no_grad():
         ob_tensor = torch.tensor(observation.copy(), dtype=torch.float)
         q_values = model(ob_tensor)
         action = np.argmax(q_values).numpy()
-        observation_next, reward, done, info = env.step(action)
-        observation = observation_next
+        observation, reward, done, info = env.step(action)
         sum_reward += reward
-        # time.sleep(1/30)
+        time.sleep(1/30)
 
 
 print("Sum reward: " + str(sum_reward))
